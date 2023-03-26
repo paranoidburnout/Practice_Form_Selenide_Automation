b184aa0bcee1fccf491b1af93506947200521b47
Lissa Vasenko <lisa.vasenko@outlook.com>
2023-03-26 17:29:05 +0300

    tests was updated using JUnit5 annotations

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -1,15 +1,88 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import utils.RandomFaker;
 import utils.UserData;
 
 public class PracticeFormTest extends TestBase {
-    @Test
-    void fillAndCheckPracticeForm() {
+
+    @CsvSource(value = {"Male", "Female", "Other"})
+    @ParameterizedTest
+    @DisplayName("Корректное отображение пола {1} выбранных при регистрации в модульном окне подтверждения")
+    @Tags({
+            @Tag("REGRESS"),
+            @Tag("UI")
+    })
+    void fillAndCheckParameterizedGenderInPracticeForm(String gender) {
         UserData userData = RandomFaker.getRandomUser();
         registrationPage.openPage()
                 .setFirstName(userData.getFirstName())
+                .setLastName(userData.getLastName())
+                .setGender(gender)
+                .setPhone(userData.getPhone())
+                .submit();
+
+        registrationPage.verifyResultsModalAppears()
+                .verifyResult("Student Name", userData.getFirstName() + " " + userData.getLastName())
+                .verifyResult("Gender", gender)
+                .verifyResult("Mobile", userData.getPhone())
+                .close();
+    }
+    @CsvFileSource(resources = "/data/firstNameAndSecondNameData.csv")
+    @ParameterizedTest(name = "Корректное отображение имени {0}, фамилии {1} введенных при регистрации в окне подтверждения")
+    @Tags({
+            @Tag("REGRESS"),
+            @Tag("UI")
+    })
+    void fillAndCheckParametrNameAndLastNameInPracticeForm(String name, String lastName) {
+        UserData userData = RandomFaker.getRandomUser();
+        registrationPage.openPage()
+                .setFirstName(name)
+                .setLastName(lastName)
+                .setGender(userData.getGender())
+                .setPhone(userData.getPhone())
+                .submit();
+
+        registrationPage.verifyResultsModalAppears()
+                .verifyResult("Student Name", name + " " + lastName)
+                .verifyResult("Gender", userData.getGender())
+                .verifyResult("Mobile", userData.getPhone())
+                .close();
+    }
+    @ValueSource(strings = {"Lissa", "Maggie", "Emma", "Dekrissa"})
+    @ParameterizedTest(name = "Корректное отображение имени со значением {0} введенного при регистрации в модальном окне подтверждения")
+    @Tag("UI")
+    void fillAndCheckParametrNameInPracticeForm(String data) {
+        UserData userData = RandomFaker.getRandomUser();
+        registrationPage.openPage()
+                .setFirstName(data)
+                .setLastName(userData.getLastName())
+                .setGender(userData.getGender())
+                .setPhone(userData.getPhone())
+                .submit();
+
+        registrationPage.verifyResultsModalAppears()
+                .verifyResult("Student Name", data + " " + userData.getLastName())
+                .verifyResult("Gender", userData.getGender())
+                .verifyResult("Mobile", userData.getPhone())
+                .close();
+    }
+
+    @Disabled("Вечно ломающийся календарь и залипающие хобби ломают тест")
+    @ValueSource(strings = {"Lissa", "Maggie", "Emma", "Dekrissa"})
+    @ParameterizedTest(name = "Корректное отображение имени со значением {0} введенного при регистрации в модальном окне подтверждения")
+    @Tag("UI")
+    void fillAndCheckPracticeForm(String data) {
+        UserData userData = RandomFaker.getRandomUser();
+        registrationPage.openPage()
+                .setFirstName(data)
                 .setLastName(userData.getLastName())
                 .setEmail(userData.getEmail())
                 .setGender(userData.getGender())
@@ -24,7 +97,7 @@ public class PracticeFormTest extends TestBase {
                 .submit();
 
         registrationPage.verifyResultsModalAppears()
-                .verifyResult("Student Name", userData.getFirstName() + " " + userData.getLastName())
+                .verifyResult("Student Name", data + " " + userData.getLastName())
                 .verifyResult("Student Email", userData.getEmail())
                 .verifyResult("Gender", userData.getGender())
                 .verifyResult("Mobile", userData.getPhone())
