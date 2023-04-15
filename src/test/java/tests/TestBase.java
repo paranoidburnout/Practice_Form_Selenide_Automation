dff978d2a71279ccfc8ab5ae2ed4bfdaa5b2468c
Lissa Vasenko <lisa.vasenko@outlook.com>
2023-04-15 22:48:27 +0300

    TestBase was updated

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,6 +18,8 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
